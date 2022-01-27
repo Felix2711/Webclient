1afb608957df7a5c5dd24e3af0e4215f0dc4b3b0
Felix Schinagl <felix.schinaglbulme.at>
2022-01-27 18:18:40 +0100

    working version 1.0

--- webclient.cpp
+++ webclient.cpp
@@ -4,4 +4,33 @@ Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+
+    m_socket = new QTcpSocket(this);
+
+    connect(m_socket, &QTcpSocket::connected, this, &Webclient::connected);
+    connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::readyRead);
+}
+
+void Webclient::connected()
+{
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_hostname + "\r\n\r\n").toLocal8Bit());
+}
+
+void Webclient::readyRead()
+{
+    resulatWindow->setText(m_socket->readAll());
+    m_socket->disconnectFromHost();
+}
+
+void Webclient::on_goButton_clicked()
+{
+    resulatWindow->clear();
+
+    m_hostname = hostname->text();
+    m_socket->connectToHost(m_hostname, m_port);
+
+    if(!m_socket->waitForConnected(5000))
+    {
+        resulatWindow->setText("Connect failed");
+    }
 }
--- webclient.h
+++ webclient.h
@@ -3,12 +3,26 @@
 
 #include "ui_webclient.h"
 
+#include <QTcpSocket>
+#include <QAbstractSocket>
+
 class Webclient : public QWidget, private Ui::Webclient
 {
     Q_OBJECT
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+
+private slots:
+    void on_goButton_clicked();
+
+private:
+    QTcpSocket *m_socket;
+    QString m_hostname;
+    unsigned short m_port = 80;
+
+    void connected();
+    void readyRead();
 };
 
 #endif // WEBCLIENT_H
